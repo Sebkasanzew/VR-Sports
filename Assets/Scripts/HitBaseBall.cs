0423af6038265420c355bab5a26b9c627b32da6b
DJ Sandra <sandra.kriemann@zanox.com>
2015-10-24 16:33:21 +0200

    fun with a baseball

---
+++ Assets/Scripts/HitBaseBall.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitBaseBall : MonoBehaviour
+{
+	public float tumble;
+
+	void OnTriggerEnter(Collider other)
+	{
+		GetComponent<Rigidbody>().velocity = -GetComponent<Rigidbody>().velocity;
+	}
+}

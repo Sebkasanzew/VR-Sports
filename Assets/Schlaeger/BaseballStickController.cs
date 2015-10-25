bb8c40951bf12067ce6e50d0a0eb4153792ba62a
vr-sports <vr-sports>
2015-10-25 16:34:13 +0100

    fix bug

--- Assets/Schlaeger/BaseballStickController.cs
+++ Assets/Schlaeger/BaseballStickController.cs
@@ -1,75 +1,89 @@
 using UnityEngine;
 using System.Collections;
 
-public class BaseballStickController : MonoBehaviour {
+public class BaseballStickController : MonoBehaviour
+{
 
-	public SixenseHands			Hand;
-	public Vector3				Sensitivity = new Vector3( 0.01f, 0.01f, 0.01f );
-	
-	protected Quaternion		m_initialRotation;
-	protected Vector3			m_initialPosition;
-	protected Vector3			m_baseControllerPosition;
+    public SixenseHands Hand;
+    public Vector3 Sensitivity = new Vector3(0.01f, 0.01f, 0.01f);
 
-    private SixenseInput.Controller controller;
-		
-	// Use this for initialization
-	protected virtual void Start() 
-	{
-		m_initialRotation = this.gameObject.transform.localRotation;
-		m_initialPosition = this.gameObject.transform.localPosition;
+    protected bool m_enabled = false;
+    protected Quaternion m_initialRotation;
+    protected Vector3 m_initialPosition;
+    protected Vector3 m_baseControllerPosition;
 
-        controller = SixenseInput.GetController(Hand);
-
-        m_baseControllerPosition = new Vector3(controller.Position.x * Sensitivity.x,
-                                               controller.Position.y * Sensitivity.y,
-                                               controller.Position.z * Sensitivity.z);
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        m_initialRotation = this.gameObject.transform.localRotation;
+        m_initialPosition = this.gameObject.transform.localPosition;
     }
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		if ( Hand == SixenseHands.UNKNOWN )
-		{
-			return;
-		}
-		
-		if ( controller != null && controller.Enabled )  
-		{		
-			UpdateObject(controller);
-		}	
-	}
-		
-	protected virtual void UpdateObject(  SixenseInput.Controller controller )
-	{
-		if (controller.GetButtonDown(SixenseButtons.TRIGGER))
-		{
-			
-			// delta controller position is relative to this point
-			
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Hand == SixenseHands.UNKNOWN)
+        {
+            return;
         }
-		
-		UpdatePosition( controller );
-		UpdateRotation( controller );
-	}
-	
-	
-	protected void UpdatePosition( SixenseInput.Controller controller )
-	{
+
+        SixenseInput.Controller controller = SixenseInput.GetController(Hand);
+        if (controller != null && controller.Enabled)
+        {
+            UpdateObject(controller);
+        }
+    }
+
+
+    void OnGUI()
+    {
+        if (!m_enabled)
+        {
+            GUI.Box(new Rect(Screen.width / 2 - 100, Screen.height - 40, 200, 30), "Press Start To Move/Rotate");
+        }
+    }
+
+
+    protected virtual void UpdateObject(SixenseInput.Controller controller)
+    {
+        if (controller.GetButtonDown(SixenseButtons.TRIGGER))
+        {
+            // enable position and orientation control
+            m_enabled = !m_enabled;
+
+            // delta controller position is relative to this point
+            m_baseControllerPosition = new Vector3(controller.Position.x * Sensitivity.x,
+                                                    controller.Position.y * Sensitivity.y,
+                                                    controller.Position.z * Sensitivity.z);
+
+            // this is the new start position
+            m_initialPosition = this.gameObject.transform.localPosition;
+        }
+
+        if (m_enabled)
+        {
+            UpdatePosition(controller);
+            UpdateRotation(controller);
+        }
+    }
+
+
+    protected void UpdatePosition(SixenseInput.Controller controller)
+    {
         Vector3 controllerPosition = new Vector3(controller.Position.x * Sensitivity.x,
                                                   controller.Position.y * Sensitivity.y,
-                                                  0);// controller.Position.z * Sensitivity.z );
+                                                  controller.Position.z * Sensitivity.z);
 
         // distance controller has moved since enabling positional control
-        Debug.Log(m_baseControllerPosition);
         Vector3 vDeltaControllerPos = controllerPosition - m_baseControllerPosition;
-		
-		// update the localposition of the object
-		this.gameObject.transform.localPosition = m_initialPosition + vDeltaControllerPos;
-	}
-	
-	
-	protected void UpdateRotation( SixenseInput.Controller controller )
-	{
-		this.gameObject.transform.localRotation = controller.Rotation * m_initialRotation;
-	}
+
+        // update the localposition of the object
+        this.gameObject.transform.localPosition = m_initialPosition + vDeltaControllerPos;
+    }
+
+
+    protected void UpdateRotation(SixenseInput.Controller controller)
+    {
+        this.gameObject.transform.localRotation = controller.Rotation * m_initialRotation;
+    }
 }
